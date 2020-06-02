b6ad2f88cd302b04363865b9483c996d81202540
Art <artelo.studio@gmail.com>
2020-06-02 01:49:18 +0000

    navigation

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,15 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
+  def create
+    Job.create(job_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:username, :work, :rate, :contact)
+  end
+
 end
