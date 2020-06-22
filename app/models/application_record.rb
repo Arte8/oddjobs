2cc489a3a806f6baccce0ab1e61abd153829a211
Art <artelo.studio@gmail.com>
2020-06-22 02:23:19 +0000

    added devise notice and alert

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+def self.search(search)
+if search
+  find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+else
+  find(:all)
+end
+end
