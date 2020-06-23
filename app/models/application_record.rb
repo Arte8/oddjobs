737827b8a8bbe303750dda446f40932affb42920
Art <artelo.studio@gmail.com>
2020-06-23 23:26:34 +0000

    added column and index to db

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
-def self.search(search)
-if search
-  find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-else
-  find(:all)
-end
-end
